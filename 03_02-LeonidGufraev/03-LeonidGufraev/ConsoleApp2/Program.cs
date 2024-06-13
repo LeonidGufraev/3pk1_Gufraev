ba24a2973c849311864da21a1b7df67090e4f333
LeonidGufraev <116651637+LeonidGufraev@users.noreply.github.com>
2024-06-13 16:39:34 +0500

    3.2

---
+++ 03_02-LeonidGufraev/03-LeonidGufraev/ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool GetVisited(int a)
+            {
+                if (visited[a] == false)
+                {
+                    visited[a] = true;
+                    return visited[a];
+                }
+                else
+                {
+                    return !visited[a];
+                }
+            }
+        }
+    }
+}
